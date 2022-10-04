327ffc6c9a2c60acf826608a7a807b2d0bb09796
iván andrés <ivantrujillo1229@gmail.com>
2022-10-03 20:21:02 -0500

    add python script

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 # This file will be a python script
 
 for _ in range(1,10):
-	print(# This file will be a python script
+	print("This file will be a python script")
 
 for _ in range(1,10):
 	print('modification.txt'*29)
